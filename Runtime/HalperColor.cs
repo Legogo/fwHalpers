23482cc2490a8a27d8570e858e876fec3a37bf90
ab <andre.berlemont@gmail.com>
2022-09-14 21:35:58 +0200

    namespace & assemb

--- Runtime/HalperColor.cs
+++ Runtime/HalperColor.cs
@@ -2,105 +2,109 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-static public class HalperColor
+namespace fwp.halpers
 {
-    public const string colorParserSeparator = ";";
-
-    public readonly static Color yellowPure = new Color(250f, 250f, 50f);
-    public readonly static Color yellowDark = new Color(200f, 200f, 50f);
-    public readonly static Color orange = new Color(255f, 102f, 50f);
-
-    /// <summary>
-    /// Retourne une Color en utilisant les paramètres [0,255] au lieu de [0,1]
-    /// </summary>
-    /// <param name="r">Le canal Rouge de la couleur.</param>
-    /// <param name="g">Le canal Vert de la couleur.</param>
-    /// <param name="b">Le canal Bleu de la couleur.</param>
-    /// <param name="a">Le canal Alpha/Transparent de la couleur.</param>
-    /// <returns></returns>
-    static public Color rgbToColor(float r, float g, float b, float a = 255f)
+    static public class HalperColor
     {
-        return new Color(
-        Mathf.InverseLerp(0f, 255f, r),
-        Mathf.InverseLerp(0f, 255f, g),
-        Mathf.InverseLerp(0f, 255f, b),
-        Mathf.InverseLerp(0f, 255f, a)
-        );
+        public const string colorParserSeparator = ";";
 
-    }//rgbToColor()
+        public readonly static Color yellowPure = new Color(250f, 250f, 50f);
+        public readonly static Color yellowDark = new Color(200f, 200f, 50f);
+        public readonly static Color orange = new Color(255f, 102f, 50f);
 
-    //static public Color htmlToColor(string hex){}
-
-    static public string parse(Color color)
-    {
-        string data = string.Empty;
-
-        for (int i = 0; i < 4; i++)
+        /// <summary>
+        /// Retourne une Color en utilisant les paramètres [0,255] au lieu de [0,1]
+        /// </summary>
+        /// <param name="r">Le canal Rouge de la couleur.</param>
+        /// <param name="g">Le canal Vert de la couleur.</param>
+        /// <param name="b">Le canal Bleu de la couleur.</param>
+        /// <param name="a">Le canal Alpha/Transparent de la couleur.</param>
+        /// <returns></returns>
+        static public Color rgbToColor(float r, float g, float b, float a = 255f)
         {
-            data += color[i].ToString();
+            return new Color(
+            Mathf.InverseLerp(0f, 255f, r),
+            Mathf.InverseLerp(0f, 255f, g),
+            Mathf.InverseLerp(0f, 255f, b),
+            Mathf.InverseLerp(0f, 255f, a)
+            );
 
-            if (i < 3)
-            {
-                data += colorParserSeparator;
-            }
-        }
+        }//rgbToColor()
 
-        return data;
+        //static public Color htmlToColor(string hex){}
 
-    }// parse()
-
-    static public Color parse(string data)
-    {
-        Color color = Color.magenta;
-
-        if (!string.IsNullOrEmpty(data))
+        static public string parse(Color color)
         {
-            string[] channels = data.Split(new string[1] { colorParserSeparator }, System.StringSplitOptions.None);
+            string data = string.Empty;
 
-            if (channels != null && channels.Length == 4)
+            for (int i = 0; i < 4; i++)
             {
-                for (int i = 0; i < channels.Length; i++)
+                data += color[i].ToString();
+
+                if (i < 3)
                 {
-                    color[i] = float.Parse(channels[i]);
+                    data += colorParserSeparator;
                 }
             }
+
+            return data;
+
+        }// parse()
+
+        static public Color parse(string data)
+        {
+            Color color = Color.magenta;
+
+            if (!string.IsNullOrEmpty(data))
+            {
+                string[] channels = data.Split(new string[1] { colorParserSeparator }, System.StringSplitOptions.None);
+
+                if (channels != null && channels.Length == 4)
+                {
+                    for (int i = 0; i < channels.Length; i++)
+                    {
+                        color[i] = float.Parse(channels[i]);
+                    }
+                }
+            }
+
+            return color;
+
+        }// parse()
+
+        /// <summary>
+        /// Retourne une Color avec le canal d'Alpha modifié.
+        /// </summary>
+        /// <param name="color">La Color cible.</param>
+        /// <param name="a">La valeur du canal Alpha [0,1]</param>
+        /// <returns>La Color avec le canal d'alpha modifié.</returns>
+        static public Color setAlpha(this Color color, float a = 1f)
+        {
+            color.a = a;
+
+            return color;
         }
 
-        return color;
+        static public Color towardAlpha(this Color color, float target = 1f, float speed = 1f)
+        {
+            color.a = Mathf.MoveTowards(color.a, target, speed);
+            return color;
+        }
 
-    }// parse()
+        /// <summary>
+        /// Retourne une Color avec la valeur d'alpha multiplié.
+        /// </summary>
+        /// <param name="color">La Color cible</param>
+        /// <param name="power">Le multiplicateur du canal d'alpha</param>
+        /// <returns>La Color ainsi modifiée</returns>
+        static public Color multAlpha(this Color color, float power = 1f)
+        {
+            color.a *= power;
 
-    /// <summary>
-    /// Retourne une Color avec le canal d'Alpha modifié.
-    /// </summary>
-    /// <param name="color">La Color cible.</param>
-    /// <param name="a">La valeur du canal Alpha [0,1]</param>
-    /// <returns>La Color avec le canal d'alpha modifié.</returns>
-    static public Color setAlpha(this Color color, float a = 1f)
-    {
-        color.a = a;
+            return color;
 
-        return color;
+        }// multAlpha()
+
     }
-
-    static public Color towardAlpha(this Color color, float target = 1f, float speed = 1f)
-    {
-        color.a = Mathf.MoveTowards(color.a, target, speed);
-        return color;
-    }
-
-    /// <summary>
-    /// Retourne une Color avec la valeur d'alpha multiplié.
-    /// </summary>
-    /// <param name="color">La Color cible</param>
-    /// <param name="power">Le multiplicateur du canal d'alpha</param>
-    /// <returns>La Color ainsi modifiée</returns>
-    static public Color multAlpha(this Color color, float power = 1f)
-    {
-        color.a *= power;
-
-        return color;
-
-    }// multAlpha()
 
 }

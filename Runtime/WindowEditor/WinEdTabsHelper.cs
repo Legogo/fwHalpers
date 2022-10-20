e286198add4e100ad82b997ddddfe65cfed71e4c
ab <andre.berlemont@gmail.com>
2022-10-20 15:53:01 +0200

    win ed draw

---
+++ Runtime/WindowEditor/WinEdTabsHelper.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace fwp.halpers
+{
+    public class WinEdTabsHelper : MonoBehaviour
+    {
+        static public GUIContent[] generateTabsDatas(string[] labels)
+        {
+            GUIContent[] modeLabels = new GUIContent[labels.Length];
+
+            //GUILayout.Label("no mode labels (x" + nms.Length + " candidates)");
+
+            for (int i = 0; i < labels.Length; i++)
+            {
+                modeLabels[i] = new GUIContent(labels[i], "tooltip");
+            }
+
+            return modeLabels;
+        }
+
+        static public int generateTabsHeader(int tabSelected, GUIContent[] tabs)
+        {
+            //GUIStyle gs = new GUIStyle(GUI.skin.button)
+            //int newTab = GUILayout.Toolbar((int)tabSelected, modeLabels, "LargeButton", GUILayout.Width(toolbarWidth), GUILayout.ExpandWidth(true));
+            int newTab = GUILayout.Toolbar((int)tabSelected, tabs, "LargeButton");
+            //if (newTab != (int)tabSelected) Debug.Log("changed tab ? " + tabSelected);
+
+            return newTab;
+        }
+    }
+}
